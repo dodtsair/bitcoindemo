cec63bf6b69a944e189380ca00205f2d26c4537a
Michael Power <me@dodtsair.com>
2020-09-20 16:41:05 -0700

    added auth

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import React, { useEffect, useState } from 'react'
 import Amplify, { API, graphqlOperation } from 'aws-amplify'
 import { createNewWatch } from './graphql/mutations'
 import { getWatchList } from './graphql/queries'
-
+import { withAuthenticator } from '@aws-amplify/ui-react'
 import awsExports from "./aws-exports";
+
 Amplify.configure(awsExports);
 
 const initialState = { name: '', bitcoinaddress: '' }
@@ -80,4 +81,4 @@ const styles = {
   button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
 }
 
-export default App
+export default withAuthenticator(App)
